d72163fbe1187cd2977d7905e493fd0bffe1b669
Rafael Cordeiro <rafaelsc@rafaelsc.net>
2023-03-03 00:35:32 -0800

    Update

--- types/three/examples/jsm/webxr/XREstimatedLight.d.ts
+++ types/three/examples/jsm/webxr/XREstimatedLight.d.ts
@@ -1,4 +1,12 @@
-import { DirectionalLight, Group, LightProbe, Texture, WebGLRenderer } from '../../../src/Three';
+import {
+    DirectionalLight,
+    Group,
+    LightProbe,
+    Texture,
+    WebGLRenderer,
+    Object3DEventMap,
+    EmptyEvent,
+} from '../../../src/Three';
 
 export class SessionLightProbe {
     xrLight: XREstimatedLight;
@@ -23,7 +31,12 @@ export class SessionLightProbe {
     dispose: () => void;
 }
 
-export class XREstimatedLight extends Group {
+export interface XREstimatedLightEventMap extends Object3DEventMap {
+    estimationstart: EmptyEvent;
+    estimationend: EmptyEvent;
+}
+
+export class XREstimatedLight extends Group<XREstimatedLightEventMap> {
     lightProbe: LightProbe;
     directionalLight: DirectionalLight;
     environment: Texture;
